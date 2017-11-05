6db55e6453e55a7165cedc82beb574c8edc29fed
AliLynne <alilynne.thompson@gmail.com>
2017-11-05 16:12:31 +0000

    game deletion

--- boardgames_api/public/index.js
+++ boardgames_api/public/index.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
     $.getJSON('/api/boardgames')
     .then(addGames)
     
+    $('#gameInput').keypress(function(event){
+        if (event.which === 13) {
+            createGame()
+        }
+    })
+    
+    $('.list').on('click', 'span', function(e){
+      e.stopPropagation()
+      deleteGame($(this).parent())
+    })
+  
+    $('.list').on('click', 'li', function(){
+      updateGame($(this))
+    })  
+    
 }) // end document.ready
 
 function addGames(games){
@@ -12,6 +27,38 @@ function addGames(games){
   } // end add games
   
 function addGame(game){
-    let newGame = $('<li class="game">' + game.title + '</li>')
+    let newGame = $('<li class="game">' + game.title + '<span>X</span></li>')
     $('.list').append(newGame)
+}
+
+function createGame(game){
+    const userInput = $('#gameInput').val()
+    $.post('/api/boardgames', {title: userInput})
+      .then(function(newGame){
+        addGame(newGame)
+        $('#gameInput').val('')
+      })
+      .catch(function(err){
+        console.log(err)
+      })
+}
+
+function updateGame(game){
+
+}
+
+function deleteGame(game){
+  let clickedId = game.data('id')
+  let clickedUrl = '/api/boardgames/' + clickedId
+  
+  $.ajax({
+    method: 'DELETE',
+    url: clickedUrl
+  })
+  .then(function(data){
+    game.remove()
+  })
+  .catch(function(err){
+    console.log(err)
+  })
 }
